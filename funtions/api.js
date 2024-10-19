0ca93e13a5fe93fe6c9ad7f4a33acd750df2aadb
bsdaoquang <bsdaoquang@gmail.com>
2024-10-20 00:36:23 +0700

    add serverless

--- funtions/api.js
+++ funtions/api.js
@@ -1,8 +1,12 @@
 /** @format */
 import express from 'express';
+import serverless from 'serverless-http';
 
 const app = express();
 
 app.get('/', (req, res) => {
 	res.send('<h1>fafafa</h1>');
 });
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
